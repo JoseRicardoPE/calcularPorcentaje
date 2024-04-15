ac780bc69ed620dd4377f09221635a68d0850793
Ricardo Pacheco <ricardo.pachecoescobar@gmail.com>
2024-04-14 19:57:03 -0500

    add logic and template

--- src/app/components/percent/percent.component.ts
+++ src/app/components/percent/percent.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-percent',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PercentComponent implements OnInit {
 
-  constructor() { }
+  showPercent = false;
+  formCalcPercent!: FormGroup
+  result: number | undefined;
+
+  constructor(private fb: FormBuilder) {
+    this.formCalcPercent = fb.group({
+      inputPercent: ['', Validators.required],
+      inputAmount: ['', Validators.required],
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  calcPercent() {
+    if (this.formCalcPercent.valid) {
+      const percent = this.formCalcPercent.get('inputPercent')?.value;
+      const amount = this.formCalcPercent.get('inputAmount')?.value;
+      this.result = (percent / 100) * amount;
+      console.log(this.result);
+      this.showPercent = true;
+    } else {
+      this.showPercent = false;
+    }
+  }
+
+  resetInputs() {
+    this.formCalcPercent.reset();
+    this.showPercent = false;
   }
 
 }
